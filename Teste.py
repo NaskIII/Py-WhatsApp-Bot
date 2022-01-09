780dbc4cc3a5c10f22fd03e6969ba064d24b1c02
Nask <33010870+NaskIII@users.noreply.github.com>
2022-01-09 01:49:03 -0300

    Added feature to read and send images

--- Teste.py
+++ Teste.py
@@ -1,13 +1,15 @@
 from core import Whatsapp
-import time
 
 
 whats = Whatsapp.Whatsapp_API(width=1280, height=720)
 whats.web(5)
-whats.contacts('Teste para a Cortana')
+whats.contacts('Creuza')
 whats.message('Olá, sou Cortana, é um prazer ver vocês.')
 whats.send_message()
-while True:
+
+quit: bool = True
+
+while quit:
     message: str = whats.read_message('!')
     if message == '!ping':
         whats.answer_message('pong')
@@ -16,6 +18,7 @@ while True:
     if message == '!quit':
         whats.answer_message('Adeus!')
         whats.quit()
+        quit = False
     if message.__contains__('!image'):
         if len(message.split()) <= 1:
             whats.answer_message('Por favor, siga as regras de imagem.')
@@ -24,5 +27,13 @@ while True:
                 whats.answer_message("Comando executado com sucesso.")
             else:
                 whats.answer_message(
+                    "Houve um erro inesperado, por favor siga as regras de imagem e tente novamente mais tarde.")
+    if message.__contains__('!simage'):
+        if len(message.split()) <= 1:
+            whats.answer_message('Por favor, siga as regras de imagem.')
+        elif len(message.split()) >= 2:
+            if whats.send_image(message.split('_')[1]):
+                whats.answer_message("Comando executado com sucesso.")
+            else:
+                whats.answer_message(
                     "Houve um erro inesperado, por favor siga as regras de imagem e tende novamente mais tarde.")
-
--- core/Whatsapp.py
+++ core/Whatsapp.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 from webdriver_manager.chrome import ChromeDriverManager
 
 import requests
 import os
 import time
+from platform import system
+import pathlib
+from io import BytesIO
 
-from webdriver_manager.utils import File
+from pyautogui import hotkey
+import win32clipboard
+from PIL import Image
 
 from core.Exceptions import InternetExeption
 
@@ -22,11 +27,15 @@ TIMEOUT: int = 5
 INIT_TIMEOUT: int = 15
 LAST_MESSAGE: str = ''
 
+HTML_CONTACTS_TEXTBOX: str = '//*[@id="side"]/div[1]/div/label/div/div[2]'
 HTML_BALOON_MESSAGE: str = 'Nm1g1'
 HTML_MESSAGE: str = '_1Gy50'
 HTML_HOUR_MESSAGE: str = 'kOrB_'
+HTML_IMAGE_BOX_CLASS: str = '_1N4rE'
+HTML_IMAGE_CLASS: str = '_3IfUe'
 HTML_XPATH_TEXTBOX: str = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]'
-HTML_SEND_BUTTON: str = '_4sWnG'
+HTML_SEND_XPATH_BUTTON: str = '//span[@data-testid="send"]'
+HTML_X_XPATH_BUTTON: str = '//span[@data-testid="x-viewer"]'
 
 
 class Whatsapp_API(object):
@@ -42,7 +51,7 @@ class Whatsapp_API(object):
         self.driver.set_window_position(
             x=window_position_x, y=window_position_y)
 
-    def check_internet_connection(self, url: str = CHECK_CONNECTION_URL):
+    def check_internet_connection(self, url: str = CHECK_CONNECTION_URL) -> None:
         """Check the Internet connection of the Host Machine"""
         try:
             requests.get(url)
@@ -60,44 +69,103 @@ class Whatsapp_API(object):
         except:
             self.driver.quit()
 
-    def contacts(self, target: str) -> bool:
-        """Go into target chat in whatsapp"""
-        chat = self.driver.find_element(by=By.XPATH,
-                                        value="//span[@title='%s']" % (target))
-        chat.click()
-        return True
-
-    def message(self, text: str) -> bool:
-        """Send text to whatsapp textbox"""
-        global HTML_XPATH_TEXTBOX
-        chat_box = self.driver.find_element(by=By.XPATH,
-                                            value=HTML_XPATH_TEXTBOX)
-        chat_box.send_keys('Cortana: %s' % (text))
-        return True
-
-    def send_message(self) -> bool:
-        """Send message"""
-        global TIMEOUT, HTML_SEND_BUTTON
+    def send_button(self) -> WebElement:
+        global TIMEOUT, HTML_SEND_XPATH_BUTTON
         try:
             element_present = EC.presence_of_element_located(
-                (By.CLASS_NAME, HTML_SEND_BUTTON))
+                (By.XPATH, HTML_SEND_XPATH_BUTTON))
             WebDriverWait(self.driver, TIMEOUT).until(element_present)
-            send_button = self.driver.find_element(
-                by=By.CLASS_NAME, value=HTML_SEND_BUTTON)
-            send_button.click()
-            return True
+            return self.driver.find_element(by=By.XPATH, value=HTML_SEND_XPATH_BUTTON)
         except TimeoutException:
-            print('TimeoutException')
-            return False
+            raise TimeoutException(
+                "Error trying to find the html element, please check html xPath."
+            )
 
-    def answer_message(self, text: str) -> bool:
+    def chat_box(self, chat_box: str) -> WebElement:
+        global TIMEOUT
+        try:
+            element_present = EC.presence_of_element_located(
+                (By.XPATH, chat_box))
+            WebDriverWait(self.driver, TIMEOUT).until(element_present)
+            return self.driver.find_element(by=By.XPATH, value=chat_box)
+        except TimeoutException:
+            raise TimeoutException(
+                "Error trying to find the html element, please check html xPath."
+            )
+
+    def find_contact(self, contact_name: str) -> WebElement:
+        try:
+            element_present = EC.presence_of_element_located(
+                (By.XPATH, "//span[@title='%s']" % (contact_name)))
+            WebDriverWait(self.driver, TIMEOUT).until(element_present)
+            return self.driver.find_element(by=By.XPATH, value="//span[@title='%s']" % (contact_name))
+        except TimeoutException:
+            raise TimeoutException(
+                "Error trying to find the html element, please check html xPath."
+            )
+
+    def close_image(self) -> WebElement:
+        global TIMEOUT, HTML_X_XPATH_BUTTON
+        try:
+            element_present = EC.presence_of_element_located(
+                (By.XPATH, HTML_X_XPATH_BUTTON))
+            WebDriverWait(self.driver, TIMEOUT).until(element_present)
+            return self.driver.find_element(
+                by=By.XPATH, value=HTML_X_XPATH_BUTTON)
+        except TimeoutException:
+            raise TimeoutException(
+                "Error trying to find the html element, please check html xPath."
+            )
+
+    def get_image_by_xpath(self, message: str) -> list[WebElement]:
+        global TIMEOUT
+        try:
+            element_present = EC.presence_of_element_located(
+                (By.XPATH, message))
+            WebDriverWait(self.driver, TIMEOUT).until(element_present)
+            return self.driver.find_elements(
+                by=By.XPATH, value=message)
+        except TimeoutException:
+            raise TimeoutException(
+                "Error trying to find the html element, please check html xPath."
+            )
+
+    def get_image_by_class_name(self, value: str) -> list[WebElement]:
+        global TIMEOUT
+
+        try:
+            element_present = EC.presence_of_element_located(
+                (By.CLASS_NAME, value))
+            WebDriverWait(self.driver, TIMEOUT).until(element_present)
+            return self.driver.find_elements(
+                by=By.CLASS_NAME, value=value)
+        except TimeoutException:
+            raise TimeoutException(
+                "Error trying to find the html element, please check html xPath."
+            )
+
+    def contacts(self, contact_name: str) -> None:
+        """Go into target chat in whatsapp"""
+        global HTML_CONTACTS_TEXTBOX
+
+        self.chat_box(HTML_CONTACTS_TEXTBOX).send_keys(contact_name)
+        self.find_contact(contact_name).click()
+
+    def message(self, text: str, bot_name: str = 'Cortana') -> None:
+        """Send text to whatsapp textbox"""
+        global HTML_XPATH_TEXTBOX
+
+        self.chat_box(HTML_XPATH_TEXTBOX).send_keys(
+            '%s: %s' % (bot_name, text))
+
+    def send_message(self) -> None:
+        """Send message"""
+        self.send_button().click()
+
+    def answer_message(self, text: str) -> None:
         """Send text to whatsapp textbox and send message"""
-        result: bool = self.message(text=text)
-        if result:
-            result = self.send_message()
-            return result
-        else:
-            return False
+        self.message(text=text)
+        self.send_button().click()
 
     def read_message(self, trigger: str = '!') -> str:
         """
@@ -108,28 +176,75 @@ class Whatsapp_API(object):
         global LAST_MESSAGE, HTML_MESSAGE, HTML_BALOON_MESSAGE, HTML_HOUR_MESSAGE
         message = self.driver.find_elements(
             by=By.CLASS_NAME, value=HTML_BALOON_MESSAGE)[-1]
-        if time.strftime(message.find_element(by=By.CLASS_NAME, value=HTML_HOUR_MESSAGE).text) >= time.strftime("%H:%M"):
-            if message.find_element(by=By.CLASS_NAME, value=HTML_MESSAGE).text[0] == trigger and message.find_element(by=By.CLASS_NAME, value=HTML_MESSAGE).text != LAST_MESSAGE:
-                _LAST_MESSAGE = message.find_element(
-                    by=By.CLASS_NAME, value=HTML_MESSAGE).text
-                print(_LAST_MESSAGE)
-                return message.find_element(by=By.CLASS_NAME, value=HTML_MESSAGE).text
+        if message.find_element(by=By.CLASS_NAME, value=HTML_MESSAGE).text[0] == trigger and message.find_element(by=By.CLASS_NAME, value=HTML_MESSAGE).text != LAST_MESSAGE:
+            _LAST_MESSAGE = message.find_element(
+                by=By.CLASS_NAME, value=HTML_MESSAGE).text
+            print(_LAST_MESSAGE)
+            return message.find_element(by=By.CLASS_NAME, value=HTML_MESSAGE).text
         return ''
 
     def read_image(self, fileName: str, triggerMessage: str, dir: str = os.getcwd()) -> bool:
+        global HTML_IMAGE_BOX_CLASS, HTML_IMAGE_CLASS
         try:
             with open(dir + '/' + fileName + '.png', 'wb') as file:
-                self.driver.find_elements(
-                    by=By.XPATH, value="//img[@alt='%s']" % (triggerMessage))[-1].click()
+                self.get_image_by_xpath(
+                    "//img[@alt='%s']" % (triggerMessage))[-1].click()
                 time.sleep(2)
-                image = self.driver.find_element(
-                    by=By.CLASS_NAME, value="_1N4rE").find_elements(by=By.CLASS_NAME, value='_3IfUe')[-1].screenshot_as_png
+                image = self.get_image_by_class_name(HTML_IMAGE_BOX_CLASS)[-1].find_elements(
+                    by=By.CLASS_NAME, value=HTML_IMAGE_CLASS)[-1].screenshot_as_png
                 file.write(image)
-                self.driver.find_element(
-                    by=By.XPATH, value='//span[@data-testid="x-viewer"]').click()
+                self.close_image().click()
                 return True
+        except IOError as err:
+            print(err)
+        except Exception as err:
+            print(err)
+
+    def copy_image(self, path: str) -> None:
+        """Copy the Image to Clipboard based on the Platform"""
+        if system().lower() == "linux":
+            if pathlib.Path(path).suffix in (".PNG", ".png"):
+                os.system(f"copyq copy image/png - < {path}")
+            elif pathlib.Path(path).suffix in (".jpg", ".JPG", ".jpeg", ".JPEG"):
+                os.system(f"copyq copy image/jpeg - < {path}")
+            else:
+                raise Exception(
+                    f"File Format {pathlib.Path(path).suffix} is not Supported!"
+                )
+        elif system().lower() == "windows":
+            image = Image.open(path)
+            output = BytesIO()
+            image.convert("RGB").save(output, "BMP")
+            data = output.getvalue()[14:]
+            output.close()
+            win32clipboard.OpenClipboard()
+            win32clipboard.EmptyClipboard()
+            win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+            win32clipboard.CloseClipboard()
+        elif system().lower() == "darwin":
+            if pathlib.Path(path).suffix in (".jpg", ".jpeg", ".JPG", ".JPEG"):
+                os.system(
+                    f"osascript -e 'set the clipboard to (read (POSIX file \"{path}\") as JPEG picture)'"
+                )
+            else:
+                raise Exception(
+                    f"File Format {pathlib.Path(path).suffix} is not Supported!"
+                )
+        else:
+            raise Exception(f"Unsupported System: {system().lower()}")
+
+    def send_image(self, dir: str) -> bool:
+        global HTML_XPATH_TEXTBOX
+        try:
+            self.chat_box(HTML_XPATH_TEXTBOX).click()
+            self.copy_image(dir)
+            hotkey("ctrl", "v")
+            self.send_button().click()
+            time.sleep(1)
+            return True
         except:
             return False
 
     def quit(self) -> None:
+        time.sleep(2)
         self.driver.quit()
